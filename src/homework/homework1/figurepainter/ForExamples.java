cad5ba5ed64d0218eff162c367609433a9f7a175
Goharik Hovhannsiyan <gog.hovhannisyan.18@bk.ru>
2022-12-21 17:15:56 +0400

    Add Array Examples from chapter 3

--- src/homework/homework1/figurepainter/ForExamples.java
+++ src/homework/homework1/figurepainter/ForExamples.java
@@ -4,12 +4,28 @@ public class ForExamples {
     public static void main(String[] args) {
 
         /*
-        *
-       * *
-      * * *
-     * * * *
-     *
-        **/
+          *
+         * *
+        * * *
+       * * * *
+        * * *
+         * *
+          *
+         *
+         **/
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j <= 5; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println("");
+        }
+
 
     }
 }
