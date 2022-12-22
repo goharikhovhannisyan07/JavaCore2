12e30cc1f52dc7cd3516500aace9c732012d8766
Goharik Hovhannsiyan <gog.hovhannisyan.18@bk.ru>
2022-12-22 17:21:58 +0400

    Add Array Example

--- src/homework/Array/ArrayExample.java
+++ src/homework/Array/ArrayExample.java
@@ -4,10 +4,11 @@ public class ArrayExample {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 0, 14, 0, 56};
-        for (int i = 1; i < array.length; i++) {
+        int[] array = {66, 2, 3, 0, 14, 0, 56};
+        for (int i = 0; i < array.length; i++) {
             if (array[i] != 0) {
                 System.out.print(array[i] + " ");
+
             }
         }
     }
