91fa7c3be586aeb4dec2448180e83b3a8c0a69c6
Goharik Hovhannsiyan <gog.hovhannisyan.18@bk.ru>
2023-01-14 17:59:04 +0400

    Add homework6 and chapter6

--- src/myexample/IfForExample.java
+++ src/myexample/IfForExample.java
@@ -7,17 +7,29 @@ public class IfForExample {
         int b = 10;
 
         if (a > b) {
-            System.out.println("a > b ");
+            System.out.println("max = " + a);
         } else if (a < b) {
-            System.out.println("a < b ");
-        } else System.out.println("a = b ");
+            System.out.println("max = " + b);
+        }
+
 
         for (int i = a; i <= b; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
 
+        int a1 = 511;
+        int b1 = 101;
+        char c = (char) a1;
+        char c1 = (char) b1;
+        System.out.println(c + " " + c1);
 
+
+        if (a > b) {
+            System.out.println("a > b ");
+        } else if (a < b) {
+            System.out.println("a < b ");
+        } else System.out.println("a = b ");
 
 
     }
