973dead361c88d6158dcf266fe4fda305ba87dbe
Goharik Hovhannsiyan <gog.hovhannisyan.18@bk.ru>
2023-01-15 17:47:30 +0400

    Add homework7

---
+++ src/homework/homework7/calculatorDemo.java
@@ -0,0 +1,14 @@
+package homework.homework7;
+
+public class calculatorDemo {
+    public static void main(String[] args) {
+
+        calculator cal = new calculator();
+        System.out.println(cal.plus(5.10, 10.0));
+        System.out.println(cal.minus(6.5, 5.6));
+        System.out.println(cal.divide(90.4, 2.0));
+        System.out.println(cal.multiply(5.0, 0.0));
+
+
+    }
+}
